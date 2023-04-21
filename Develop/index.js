58d3873d18c1264e51ff72c84b6840f5a0f6e09b
samuel de jesus <samue@MacBook-Air.local>
2023-04-21 11:37:42 -0400

    added links for badges and the format for the readme as well as the questions asked for to the user

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,125 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name:'title',
+        message:'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name:'description',
+        message:'What is the reason for your project, what are the who, why, what?'
+    },
+    {
+        type: 'checkbox',
+        name: 'tableOfContents',
+        message: 'Select the sections you want to include in the table of contents:',
+        choices: [
+          {
+            name: 'Installation',
+            value: 'installation'
+          },
+          {
+            name: 'Usage',
+            value: 'usage'
+          },
+          {
+            name: 'Credits',
+            value: 'credits'
+          },
+          {
+            name: 'License',
+            value: 'license'
+          },
+         {
+            name: 'Features',
+            value: 'features'
+         },
+         {
+            name:'How-to-contribute',
+            value: 'how to contribute'
+         },
+         {
+            name:'Test',
+            value:'test'
+         }
+
+        ]
+    },
+    {
+        type: 'input',
+        name:'Installation',
+        message:'Is there any installation required?'
+    },
+    {
+        type: 'input',
+        name:'usages',
+        message:'How will someone use your project?'
+    },
+    {
+        type: 'input',
+        name:'credits',
+        message:'anyone you want to credit?'
+    },
+    {
+        type: 'input',
+        name:'Features',
+        message:'Is their any features you would like include?'
+    },
+    {
+        type: 'input',
+        name:'Contributions',
+        message:'how can people contribute to your project?'
+    },
+    {
+        type: 'input',
+        name:'Tests',
+        message:'Any tests you wanna include'
+    },
+    {
+        type: 'list',
+        name: 'License',
+        choices:['MIT','ISC','GNUPLV3',]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filename, data) {
+    fs.writeFile('README.md', data, (err) => {
+        if (err) throw err;
+        console.log('README.md file generated successfully!');
+      });
+      
+    inquirer.prompt(questions)
+    .then((answers) => {
+        const {title, description, tableOfContents, Installations, usage, credits, Features, Contributions, Tests, License } = answers
+        const data = `title: ${title}\ndescription: ${description}\ntableOfContents: ${tableOfContents}\nInstallations: ${Installations}\nusage: ${usage}\ncredits: ${credits}\nFeatures: ${Features}\nContributions: ${Contributions}\nTests: ${Tests}\nLicense: ${License}`;
+      writeToFile('README.md', data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        const markdown = generateMarkdown(data);
+        writeToFile('README.md', markdown);
+      });
+}
 
 // Function call to initialize app
 init();
-//reerfeff
+
+
+
+
+
+
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,90 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'MIT'){
+  return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
 
+} else if (license === 'ISC'){  
+  return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+
+} else if (license === 'GNUPLV3') {
+  return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+
+} else {
+
+  return '';
+}
+
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT';
+  } else if (license === 'ISC') {
+    return 'https://opensource.org/licenses/ISC';
+  } else if (license === 'GNUPLV3') {
+    return 'https://www.gnu.org/licenses/gpl-3.0';
+  } else {
+    return '';
+  }
+}
+
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license. ${renderLicenseBadge(license)}`
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Description
+
+${data.description}
+
+${renderLicenseSection(data.license)}
+
+## Table of Contents
+
+${data.tableOfContents.includes('installation') ? `- [Installation](#installation)` : ''}
+${data.tableOfContents.includes('usage') ? `- [Usage](#usage)` : ''}
+${data.tableOfContents.includes('credits') ? `- [Credits](#credits)` : ''}
+${data.tableOfContents.includes('features') ? `- [Features](#features)` : ''}
+${data.tableOfContents.includes('how to contribute') ? `- [How to Contribute](#how-to-contribute)` : ''}
+${data.tableOfContents.includes('test') ? `- [Test](#test)` : ''}
+
+${data.Installation ? `## Installation
+
+${data.Installation}` : ''}
+
+${data.usages ? `## Usage
+
+${data.usages}` : ''}
+
+${data.credits ? `## Credits
+
+${data.credits}` : ''}
+
+${data.Features ? `## Features
+
+${data.Features}` : ''}
+
+${data.Contributions ? `## How to Contribute
+
+${data.Contributions}` : ''}
+
+${data.Tests ? `## Tests
+
+${data.Tests}` : ''}
 
 `;
 }
